030e2ce209d48ce195e9c461b38725c3ecb0f842
daisuke8000 <daisuke12108000@gmail.com>
2022-01-27 04:00:49 +0900

    matchController fixed

--- src/commands/matchAddFaker.go
+++ src/commands/matchAddFaker.go
@@ -8,21 +8,35 @@ import (
 
 func main() {
 	database.Connect()
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		match := models.Match{
-			MatchUser01:      rand.Intn(100-100) + 100,
+			MatchUser01Id:    rand.Intn(100) + 1,
 			MatchUser01Point: rand.Intn(50000-40000) + 40000,
 			MatchUser01Rank:  1,
-			MatchUser02:      rand.Intn(100-100) + 100,
+			MatchUser02Id:    rand.Intn(100) + 1,
 			MatchUser02Point: rand.Intn(40000-30000) + 30000,
 			MatchUser02Rank:  2,
-			MatchUser03:      rand.Intn(100-100) + 100,
+			MatchUser03Id:    rand.Intn(100) + 1,
 			MatchUser03Point: rand.Intn(20000-10000) + 10000,
 			MatchUser03Rank:  3,
-			MatchUser04:      rand.Intn(100-100) + 100,
+			MatchUser04Id:    rand.Intn(100) + 1,
 			MatchUser04Point: rand.Intn(15000-5000) + 5000,
 			MatchUser04Rank:  4,
 		}
+
 		database.DB.Create(&match)
+
+		Ids, Points, Ranks := match.CreateHistory()
+
+		for v := 0; v <= 3; {
+			history := models.History{
+				Point:  Points[v],
+				Rank:   Ranks[v],
+				UserId: uint(Ids[v]),
+				Match:  match,
+			}
+			database.DB.Create(&history)
+			v++
+		}
 	}
 }
--- src/models/match.go
+++ src/models/match.go
@@ -2,22 +2,22 @@ package models
 
 type Match struct {
 	Model
-	MatchUser01      int `json:"match_user_id_01"`
-	MatchUser01Point int `json:"match_user_01_point"`
-	MatchUser01Rank  int `json:"match_user_01_rank"`
-	MatchUser02      int `json:"match_user_id_02"`
-	MatchUser02Point int `json:"match_user_02_point"`
-	MatchUser02Rank  int `json:"match_user_02_rank"`
-	MatchUser03      int `json:"match_user_id_03"`
-	MatchUser03Point int `json:"match_user_03_point"`
-	MatchUser03Rank  int `json:"match_user_03_rank"`
-	MatchUser04      int `json:"match_user_id_04"`
-	MatchUser04Point int `json:"match_user_04_point"`
-	MatchUser04Rank  int `json:"match_user_04_rank"`
+	MatchUser01Id    int `json:"match_user01_id"`
+	MatchUser01Point int `json:"match_user01_point"`
+	MatchUser01Rank  int `json:"match_user01_rank"`
+	MatchUser02Id    int `json:"match_user02_id"`
+	MatchUser02Point int `json:"match_user02_point"`
+	MatchUser02Rank  int `json:"match_user02_rank"`
+	MatchUser03Id    int `json:"match_user03_id"`
+	MatchUser03Point int `json:"match_user03_point"`
+	MatchUser03Rank  int `json:"match_user03_rank"`
+	MatchUser04Id    int `json:"match_user04_id"`
+	MatchUser04Point int `json:"match_user04_point"`
+	MatchUser04Rank  int `json:"match_user04_rank"`
 }
 
 func (m *Match) CreateHistory() (userIdsSlice [4]int, userPointsSlice [4]int, userRankSlice [4]int) {
-	userIdsSlice = [4]int{m.MatchUser01, m.MatchUser02, m.MatchUser03, m.MatchUser04}
+	userIdsSlice = [4]int{m.MatchUser01Id, m.MatchUser02Id, m.MatchUser03Id, m.MatchUser04Id}
 	userPointsSlice = [4]int{m.MatchUser01Point, m.MatchUser02Point, m.MatchUser03Point, m.MatchUser04Point}
 	userRankSlice = [4]int{m.MatchUser01Rank, m.MatchUser02Rank, m.MatchUser03Rank, m.MatchUser04Rank}
 	return userIdsSlice, userPointsSlice, userRankSlice
